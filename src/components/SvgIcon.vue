25907d3e878d522f2378064edccf07f3ad502775
antrioe <antrioe@gmali.com>
2023-04-22 15:47:17 +0800

    🐸

--- src/components/SvgIcon.vue
+++ src/components/SvgIcon.vue
@@ -1,15 +1,19 @@
 <script setup lang='ts'>
-const props = withDefaults(defineProps<{ prefix?: string; name: string; color?: string }>(), {
+const props = withDefaults(defineProps<{ prefix?: string; name: string; color?: string; width?: number; height?: number }>(), {
   prefix: 'icon',
   color: '#333',
+  width: 1,
+  height: 1,
 })
 const symbolId = computed(() => `#${props.prefix}-${props.name}`)
 </script>
 
 <template>
-  <svg aria-hidden="true" v-bind="$attrs" :style="{ width: '1em', height: '1em' }">
+  <svg aria-hidden="true" v-bind="$attrs" :style="{ width: `${props.width}em`, height: `${props.height}em` }">
     <use :xlink:href="symbolId" :fill="props.color" />
   </svg>
 </template>
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+
+</style>

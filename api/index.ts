1b45cca51d952f4df77dd0c37fae49a7061a8c35
antrioe <antrioe@gmali.com>
2023-06-06 17:13:36 +0800

    ci(deps): 更新

--- api/index.ts
+++ api/index.ts
@@ -4,5 +4,11 @@ const app = express()
 app.get('/api', (req, res) => {
   res.send('Hello Mock API!')
 })
-
+app.get('/info', (req, res) => {
+  const cityHeader = req.headers['x-vercel-ip-city'] as string
+  const city = cityHeader ? decodeURIComponent(cityHeader) : '-'
+  const ipHeader = req.headers['x-forwarded-for'] as string
+  const ip = ipHeader ? ipHeader.split(',')[0] : '-'
+  res.send({ city, ip })
+})
 export default app

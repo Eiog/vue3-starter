d3fe5ed768d9ca296af49a0dfab94c35a12835b4
antrioe <antrioe@gmali.com>
2023-06-12 17:56:26 +0800

    ci(deps): 更新依赖

--- src/test/imports.test.ts
+++ src/test/imports.test.ts
@@ -1,3 +1,5 @@
+import { describe, expect, test } from 'vitest'
+
 describe('import vue components', () => {
   test('normal imports as expected', async () => {
     const cmp = await import('../App.vue')
